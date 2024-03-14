69ea0e9e4101dd29f933ef531cca3b060fff419f
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2024-03-14 19:31:57 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 
-import Contact from "../components/Contact";
-import AboutMe from "../components/AboutMe";
-import Footer from "../components/Footer";
-import Intro from "../components/Intro";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
+import Contact from "./components/Contact";
+import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
+import Intro from "./components/Intro";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 
 export default function Home() {
